7c9d99fd7b3a85f87d325f18d4b88d7aea25542d
mhanifazmi <hanifazmi23@gmail.com>
2023-04-10 19:32:19 +0800

    Change logo, title and meta

--- application/views/master/index.php
+++ application/views/master/index.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+if ($page['subtitle'] != null) {
+    $page_title = strtoupper(SYSTEM_NAME . " | " . $page['title'] . ' - ' . $page['subtitle']);
+} else {
+    $page_title = strtoupper(SYSTEM_NAME . " | " . $page['title']);
+}
+?>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content=" A WMS helps businesses manage their warehouse operations with greater efficiency and accuracy. Keep track of data, and save time with real-time visibility. Discover how a WMS can benefit your business.">
+    <meta name="keywords" content="wms, saratec, system">
     <meta name="author" content="pixelstrap">
     <link rel="icon" href="../assets/images/favicon.png" type="image/x-icon">
     <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon">
-    <title>Cuba - Premium Admin Template</title>
+    <title><?= $page_title ?></title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
