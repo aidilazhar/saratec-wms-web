ec08fbdd5db8754a3214e17b9e83b8d92e152aef
mhanifazmi <hanifazmi23@gmail.com>
2023-04-29 20:18:11 +0800

    Add utility helper and controller and model

--- application/controllers/Utility.php
+++ application/controllers/Utility.php
@@ -1,13 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Authentication extends CI_Controller
+class Utility extends CI_Controller
 {
     function __construct()
     {
         parent::__construct();
-
-        $this->load->model('Authentication_model');
     }
 
     public function encode($id)
