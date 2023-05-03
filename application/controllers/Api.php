fb444e7c9fe8f34ca09e4af5e0704d2b1cdabf91
mhanifazmi <hanifazmi23@gmail.com>
2023-05-03 11:19:14 +0800

    Fix CORS issues

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,443 +1,13 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS, POST");
 
 class Api extends CI_Controller
 {
     function __construct()
     {
-        $this->title = "Wires";
-        $this->wires = [
-            [
-                "id" => 1,
-                "name" => "Wire 1",
-                'package_id' => 3,
-                'drum_id' => 2,
-                'size' => 2.5,
-                'brand' => 'SUPA75',
-                'grade' => 'A',
-                'manufacturer' => "ABC Sdn. Bhd.",
-                'trial' => [
-                    'date' => date('Y-m-d'),
-                    'operator_id' => 2,
-                    'operator' => [
-                        "id" => 2,
-                        "name" => "Barry",
-                        "username" => "barry",
-                        "contact" => rand(100000000, 999999999),
-                        'role_id' => 3,
-                        "role" => [
-                            "id" => 3,
-                            "name" => "Operator",
-                        ],
-                        "status" => "Active",
-                        'email' => 'barry@yopmail.com'
-                    ],
-                    'supervisor_id' => 4,
-                    'supervisor' => [
-                        "id" => 4,
-                        "name" => "David",
-                        "username" => "david",
-                        "contact" => rand(100000000, 999999999),
-                        'role_id' => 2,
-                        "role" => [
-                            "id" => 2,
-                            "name" => "Supervisor",
-                        ],
-                        "status" => "Inactive",
-                        'email' => 'david@yopmail.com'
-                    ],
-                    'client_id' => 1,
-                    'client' => [
-                        "id" => 1,
-                        "name" => "VESTIGO",
-                    ],
-                    'package_id' => 2,
-                    'package' => [
-                        "id" => 2,
-                        "name" => "WSU 2",
-                    ],
-                    'drum_id' => 2,
-                    'drum' => [
-                        "id" => 2,
-                        "name" => "02",
-                    ],
-                    'wrap_test' => 'pass',
-                    'pull_test' => "1,000 - 1,999 lbs",
-                    'x_inch' => 1,
-                    'y_inch' => 3,
-                    'cut_off' => 2,
-                    'well_name' => "TUA-14L",
-                    'job_type_id' => 14,
-                    'job_type' => [
-                        "id" => 14,
-                        "name" => "FishingOperation",
-                    ],
-                    'jar_no' => 10,
-                    'max_pull' => 2,
-                    'is_smart_monitor_logged' => 1,
-                    'remark' => "PASS",
-                ],
-            ],
-        ];
-
-        $this->trials = [
-            [
-                'id' => 1,
-                'wire_id' => 1,
-                'date' => date('d M Y'),
-                'time' => date('h:i A'),
-                'operator_id' => 2,
-                'operator' => [
-                    "id" => 2,
-                    "name" => "Barry",
-                    "username" => "barry",
-                    "contact" => rand(100000000, 999999999),
-                    'role_id' => 3,
-                    "role" => [
-                        "id" => 3,
-                        "name" => "Operator",
-                    ],
-                    "status" => "Active",
-                    'email' => 'barry@yopmail.com'
-                ],
-                'supervisor_id' => 4,
-                'supervisor' => [
-                    "id" => 4,
-                    "name" => "David",
-                    "username" => "david",
-                    "contact" => rand(100000000, 999999999),
-                    'role_id' => 2,
-                    "role" => [
-                        "id" => 2,
-                        "name" => "Supervisor",
-                    ],
-                    "status" => "Inactive",
-                    'email' => 'david@yopmail.com'
-                ],
-                'client_id' => 1,
-                'client' => [
-                    "id" => 1,
-                    "name" => "VESTIGO",
-                ],
-                'package_id' => 2,
-                'package' => [
-                    "id" => 2,
-                    "name" => "WSU 2",
-                ],
-                'drum_id' => 2,
-                'drum' => [
-                    "id" => 2,
-                    "name" => "02",
-                ],
-                'wrap_test' => 'pass',
-                'pull_test' => "1,000 - 1,999 lbs",
-                'x_inch' => 1,
-                'y_inch' => 3,
-                'cut_off' => 2,
-                'well_name' => "TUA-14L",
-                'job_type_id' => 14,
-                'job_type' => [
-                    "id" => 14,
-                    "name" => "FishingOperation",
-                ],
-                'jar_no' => 10,
-                'max_pull' => 2,
-                'is_smart_monitor_logged' => 1,
-                'remark' => "PASS",
-            ]
-        ];
-
-        $this->supervisors = [
-            [
-                "id" => 1,
-                "name" => "Ali",
-                "username" => "ali",
-                "contact" => rand(100000000, 999999999),
-                'role_id' => 2,
-                "role" => [
-                    "id" => 2,
-                    "name" => "Supervisor",
-                ],
-                "status" => "Active",
-                'email' => 'ali@yopmail.com'
-            ],
-            [
-                "id" => 4,
-                "name" => "David",
-                "username" => "david",
-                "contact" => rand(100000000, 999999999),
-                'role_id' => 2,
-                "role" => [
-                    "id" => 2,
-                    "name" => "Supervisor",
-                ],
-                "status" => "Inactive",
-                'email' => 'david@yopmail.com'
-            ],
-        ];
-
-        $this->operators = [
-            [
-                "id" => 1,
-                "name" => "Ali",
-                "username" => "ali",
-                "contact" => rand(100000000, 999999999),
-                'role_id' => 2,
-                "role" => [
-                    "id" => 2,
-                    "name" => "Supervisor",
-                ],
-                "status" => "Active",
-                'email' => 'ali@yopmail.com'
-            ],
-            [
-                "id" => 3,
-                "name" => "Charlie",
-                "username" => "charlie",
-                "contact" => rand(100000000, 999999999),
-                'role_id' => 3,
-                "role" => [
-                    "id" => 3,
-                    "name" => "Operator",
-                ],
-                "status" => "Active",
-                'email' => 'charlie@yopmail.com'
-            ],
-        ];
-
-        $this->job_types = [
-            [
-                "id" => 1,
-                "name" => "SpoolingRespooling",
-            ],
-            [
-                "id" => 2,
-                "name" => "TccTagTdGRingTDrift",
-            ],
-            [
-                "id" => 3,
-                "name" => "WireScratcherRun",
-            ],
-            [
-                "id" => 4,
-                "name" => "FlapperProbeCheck",
-            ],
-            [
-                "id" => 5,
-                "name" => "SetRetInsValveString",
-            ],
-            [
-                "id" => 6,
-                "name" => "SetRetTbgStopPackOff",
-            ],
-            [
-                "id" => 7,
-                "name" => "SetRetCatcherSub",
-            ],
-            [
-                "id" => 8,
-                "name" => "SetRetGLVODummy",
-            ],
-            [
-                "id" => 9,
-                "name" => "SetRetPlugProng",
-            ],
-            [
-                "id" => 10,
-                "name" => "OpenCloseSSD",
-            ],
-            [
-                "id" => 11,
-                "name" => "GaugesLoggingGRCCL",
-            ],
-            [
-                "id" => 12,
-                "name" => "PerfTbgPunchCut",
-            ],
-            [
-                "id" => 13,
-                "name" => "BroachRepairTubing",
-            ],
-            [
-                "id" => 14,
-                "name" => "FishingOperation",
-            ],
-            [
-                "id" => 15,
-                "name" => "OtherRelatedJobs",
-            ],
-            [
-                "id" => 16,
-                "name" => "WireRectification",
-            ],
-        ];
-
-        $this->packages = [
-            [
-                "id" => 1,
-                "name" => "WSU 1",
-            ],
-            [
-                "id" => 2,
-                "name" => "WSU 2",
-            ],
-            [
-                "id" => 3,
-                "name" => "WSU 3",
-            ],
-            [
-                "id" => 4,
-                "name" => "WSU 4",
-            ],
-            [
-                "id" => 5,
-                "name" => "WSU 5",
-            ],
-            [
-                "id" => 6,
-                "name" => "WSU 6",
-            ],
-            [
-                "id" => 7,
-                "name" => "WSU 7",
-            ],
-        ];
-
-        $this->companies = [
-            [
-                "id" => 1,
-                "name" => "VERTIGO",
-                'clients' => [
-                    [
-                        'id' => 1,
-                        'name' => 'Petronas',
-                    ],
-                    [
-                        'id' => 2,
-                        'name' => 'Oil',
-                    ],
-                    [
-                        'id' => 3,
-                        'name' => 'Gas',
-                    ],
-                ]
-            ],
-            [
-                "id" => 2,
-                "name" => "EMEPMI",
-                'clients' => [
-                    [
-                        'id' => 4,
-                        'name' => 'Petron',
-                    ],
-                    [
-                        'id' => 5,
-                        'name' => 'Caltex',
-                    ],
-                ]
-            ],
-            [
-                "id" => 3,
-                "name" => "SHELL",
-                'clients' => [
-                    [
-                        'id' => 4,
-                        'name' => 'FIVE',
-                    ],
-                    [
-                        'id' => 5,
-                        'name' => 'Caltex',
-                    ],
-                ]
-            ],
-        ];
-
-        $this->job_types = [
-            [
-                "id" => 1,
-                "name" => "SpoolingRespooling",
-            ],
-            [
-                "id" => 2,
-                "name" => "TccTagTdGRingTDrift",
-            ],
-            [
-                "id" => 3,
-                "name" => "WireScratcherRun",
-            ],
-            [
-                "id" => 4,
-                "name" => "FlapperProbeCheck",
-            ],
-            [
-                "id" => 5,
-                "name" => "SetRetInsValveString",
-            ],
-            [
-                "id" => 6,
-                "name" => "SetRetTbgStopPackOff",
-            ],
-            [
-                "id" => 7,
-                "name" => "SetRetCatcherSub",
-            ],
-            [
-                "id" => 8,
-                "name" => "SetRetGLVODummy",
-            ],
-            [
-                "id" => 9,
-                "name" => "SetRetPlugProng",
-            ],
-            [
-                "id" => 10,
-                "name" => "OpenCloseSSD",
-            ],
-            [
-                "id" => 11,
-                "name" => "GaugesLoggingGRCCL",
-            ],
-            [
-                "id" => 12,
-                "name" => "PerfTbgPunchCut",
-            ],
-            [
-                "id" => 13,
-                "name" => "BroachRepairTubing",
-            ],
-            [
-                "id" => 14,
-                "name" => "FishingOperation",
-            ],
-            [
-                "id" => 15,
-                "name" => "OtherRelatedJobs",
-            ],
-            [
-                "id" => 16,
-                "name" => "WireRectification",
-            ],
-        ];
-
-        $this->drums = [
-            [
-                "id" => 1,
-                "name" => "01",
-            ],
-            [
-                "id" => 2,
-                "name" => "02",
-            ],
-            [
-                "id" => 3,
-                "name" => "03",
-            ],
-            [
-                "id" => 4,
-                "name" => "04",
-            ],
-        ];
-
         parent::__construct();
         $this->load->helper('url');
         $this->load->helper('form');
@@ -579,5 +149,70 @@ class Api extends CI_Controller
         $results = $this->Wire_model->store($data);
 
         echo json_encode($results);
+    }
+
+    public function chatgpt()
+    {
+        // $api_key = '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';
+        // $endpoint = 'https://api.openai.com/v1/models';
+
+        // // Set up the request headers
+        // $headers = array(
+        //     'Content-Type: application/json',
+        //     'Authorization: Bearer ' . $api_key
+        // );
+
+        // // Send the HTTP request to the OpenAI API
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $endpoint);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        // $response = curl_exec($ch);
+        // curl_close($ch);
+
+        // // Parse the JSON response and extract the model IDs
+        // $json = json_decode($response, true);
+        // $models = $json['data'];
+        // $model_ids = array();
+        // foreach ($models as $model) {
+        //     $model_ids[] = $model['id'];
+        // }
+
+        // print_r($model_ids);
+
+        $api_key = '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';
+        $model_id = 'ada';
+        $endpoint = 'https://api.openai.com/v1/completions';
+
+        // Set up the request headers
+        $headers = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $api_key
+        );
+
+        // Set up the request data
+        $data = array(
+            'prompt' => 'Hello, how are you today?',
+            'max_tokens' => 50,
+            'temperature' => 0.5,
+            'model' => $model_id
+        );
+
+        // Send the HTTP request to the OpenAI API
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        // Parse the JSON response and extract the generated text
+        $json = json_decode($response, true);
+        //$generated_text = $json['choices'][0]['text'];
+
+        print_r($json);
+        //echo $generated_text;
     }
 }
